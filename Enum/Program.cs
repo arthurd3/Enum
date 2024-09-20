3a8ff15ebbf2dba210d89e3825760523d42129f3
arthur <138174613+arthurd3@users.noreply.github.com>
2024-09-20 12:56:52 -0300

    Enum C#
    Treino C#

---
+++ Enum/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+class ENUM {
+    enum DiasSemana{Domingo , Segunda , Terça , Quarta , Quinta , Sexta , Sabado};
+
+    static void Main(string[] args)
+    {
+        int escolher;
+
+        Console.WriteLine("Escolha um Dia Da Semana : 1 a 7");
+        escolher = int.Parse(Console.ReadLine());
+
+        while (escolher > 7 || escolher < 1)
+        {
+            
+            Console.Clear();
+            Console.WriteLine("Erro Numero Invalido");
+            Console.WriteLine("Escolha um Dia Da Semana : 1 a 7");
+            escolher = int.Parse(Console.ReadLine());
+            Console.Clear();
+            
+        }
+
+
+        if (escolher == 1){
+            DiasSemana dia0 = (DiasSemana)0;
+            Console.WriteLine($"Dia Da Semana escolhido : {dia0}");
+        }else{
+            DiasSemana dia = (DiasSemana)escolher;
+            Console.WriteLine($"Dia Da Semana escolhido : {dia}");
+            
+        }
+    }
+}
